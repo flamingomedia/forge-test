e0e598da89a07eb48d0eaf1ad7bb34d8736b71fe
Filippo Donadoni <f.donadoni@flamingomedia.it>
2016-08-11 11:48:55 +0200

    Ho messo Luca

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 di Filippo</div>
+                <div class="title">Laravel 5 di Luca</div>
             </div>
         </div>
     </body>
